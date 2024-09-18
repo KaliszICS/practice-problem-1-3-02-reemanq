87496075e1a9de3ca8ddb833ac8208cc843c60ee
reemanq <reem.ansar@student.tdsb.on.ca>
2024-09-18 17:55:06 +0000

    hi

--- PP1_3.py
+++ PP1_3.py
@@ -7,16 +7,31 @@ def q1():
 * * * * * *
  * * * * * *
 * * * * * *)
+  
+  print (* * * * *
+*       *
+*       *
+*       *
+* * * * *)
+  
+  print (* * * * * *
+ *       *
+  *     *
+   *   *
+    * *
+     *)
   #Remember your code must be indented once
 
 def q2():
+  print(Hello+World)
   #Write Assignment code here
-  
+
 def q3():
+  word = "Man"
+  word = "Board"
+  word = "Man Overboard"
   #Write Assignment code here
   
-def q4():
-  #Write Assignment code here
 
 #Do not alter anything after this line
 q1()
