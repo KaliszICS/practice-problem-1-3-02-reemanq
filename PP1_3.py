17658fa1c12257ec682b27b5d27405f522fe1b75
reemanq <reem.ansar@student.tdsb.on.ca>
2024-11-04 20:36:09 -0500

    Update PP1_3.py

--- PP1_3.py
+++ PP1_3.py
@@ -1,7 +1,7 @@
 
 
 def q1():
-  print(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *")
+  print("* * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *")
   print("* * * * *\n*       *\n*       *\n*       *\n* * * * *")
   print("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *")
   #Remember your code must be indented once
